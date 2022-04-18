3c4a35dace0c776edd26f0900749afe141030d21
Mai Trung Kiên <maitrungkien1910@gmail.com>
2022-04-18 20:21:28 +0700

    fix data type error

--- tohop/tohop.cpp
+++ tohop/tohop.cpp
@@ -2,14 +2,14 @@
 #include <chrono>
 using namespace std;
 
-int dequykonho(int k, int n)
+long long dequykonho(int k, int n)
 {
   if (k == 0 || k == n)
     return 1;
   return dequykonho(k, n - 1) + dequykonho(k - 1, n - 1);
 }
 
-int dequyconho(int k, int n, int d[][100])
+long long dequyconho(int k, int n, long long d[][100])
 {
   if (k == 0 | k == n)
     return 1;
@@ -21,17 +21,17 @@ int dequyconho(int k, int n, int d[][100])
 
 int main()
 {
-  int d[100][100];
+  long long d[100][100];
   for (int i = 0; i < 100; i++)
     for (int j = 0; j < 100; j++)
       d[i][j] = 0;
   int ck[] = {10, 20, 20, 30, 40};
   int nk[] = {20, 35, 40, 50, 60};
-  for (int i = 0; i < 5; i++)
+  for (int i = 1; i < 2; i++)
   {
     auto start = std::chrono::system_clock::now().time_since_epoch();
     auto nss = std::chrono::duration_cast<std::chrono::nanoseconds>(start);
-    cout << dequyconho(ck[i], nk[i], d) << "  ";
+    cout << dequykonho(ck[i], nk[i]) << "  ";
     auto end = std::chrono::system_clock::now().time_since_epoch();
     auto nse = std::chrono::duration_cast<std::chrono::nanoseconds>(end);
     cout << (nse.count() - nss.count()) << endl;
